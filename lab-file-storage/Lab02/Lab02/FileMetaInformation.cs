2f06c258933c7fbf762884ca9cdd1ea08fd20ebd
GODELTECH\r.vorobyov <r.vorobyov@godeltech.com>
2021-01-27 12:34:25 +0300

    Add FileMetaInformation class

---
+++ lab-file-storage/Lab02/Lab02/FileMetaInformation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab02
+{
+    
+    public class FileMetaInformation
+    {
+        public string FileName { get; set; }
+        public string PathToFile { get; set; }
+        public string Extension { get; set; }
+        public uint Size { get; set; }
+        public DateTime CreationDate { get; set; }
+        public uint DownloadsCounter { get; set; }
+
+        public FileMetaInformation(string fileName, string pathToFile, string extension, uint size)
+        {
+            FileName = fileName;
+            PathToFile = pathToFile;
+            Extension = extension;
+            Size = size;
+            CreationDate = DateTime.Now;
+            DownloadsCounter = 0;
+        }
+    }
+}
