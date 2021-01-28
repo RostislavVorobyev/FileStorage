ddfcfd88a8f4a441504b60588f375de0df738b49
GODELTECH\r.vorobyov <r.vorobyov@godeltech.com>
2021-01-28 12:46:56 +0300

    Implement command parser

---
+++ lab-file-storage/Lab02/Lab02/CommandParser.cs
@@ -0,0 +1,24 @@
+﻿using Lab02.Commands;
+using System;
+
+namespace Lab02
+{
+    internal class CommandParser
+    {
+        internal static ICommand Parse(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                throw new FormatException("Boss, it's fiasco.");
+            };
+            string commandName = args[1] + args[0];
+            Type type = Type.GetType($"Lab02.Commands.{commandName}", true, true);
+            ICommand parsedCommand = (ICommand)Activator.CreateInstance(type);
+            for (int i = 2; i < args.Length; i++)
+            {
+                parsedCommand.Options.Add(args[i]);
+            }
+            return parsedCommand;
+        }
+    }
+}
