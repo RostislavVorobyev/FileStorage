5d9d54471ad6ed9dccc36d49455298e6357504c3
GODELTECH\r.vorobyov <r.vorobyov@godeltech.com>
2021-02-22 11:26:24 +0300

    [Lab03] Replace AvailableCommands enum with constansts in parser

--- lab-file-storage/LabFileStorage.UI/Util/Parser/AuthorizeCommandParser.cs
+++ lab-file-storage/LabFileStorage.UI/Util/Parser/AuthorizeCommandParser.cs
@@ -6,15 +6,16 @@ namespace LabFileStorage.UI.Util
 {
     internal class AuthorizeCommandParser : CommandLineParser
     {
+        private const string userLoginCommand = "user login";
+
         internal override ICommand Parse(string userInput)
         {
             string[] inputArguments = SplitInputArguments(userInput);
-            string commandName = $"{inputArguments[0]}{inputArguments[1]}".ToLower();
+            string commandName = $"{inputArguments[0]} {inputArguments[1]}".ToLower();
             List<string> commandOptions = ParseCommandOptions(inputArguments);
-            Enum.TryParse(commandName, true, out AvailableCommands command);
-            switch (command)
+            switch (commandName)
             {
-                case AvailableCommands.UserLogin: return new LoginUser(commandOptions);
+                case userLoginCommand: return new LoginUser(commandOptions);
                 default: throw new Exception("Use user login --l <login> --p <password> to log in.");
             }
         }
--- lab-file-storage/LabFileStorage.UI/Util/Parser/FileCommandParser.cs
+++ lab-file-storage/LabFileStorage.UI/Util/Parser/FileCommandParser.cs
@@ -8,6 +8,12 @@ namespace LabFileStorage.UI.Util
     internal class FileCommandParser : CommandLineParser
     {
         private readonly IFileService _fileService;
+        private const string fileDeleteCommand = "file delete";
+        private const string fileDownloadCommand = "file download";
+        private const string fileInfoCommand = "file info";
+        private const string fileMoveCommand = "file move";
+        private const string fileUploadCommand = "file upload";
+        private const string userInfoCommand = "user info";
 
         public FileCommandParser(IFileService fileService)
         {
@@ -17,28 +23,27 @@ namespace LabFileStorage.UI.Util
         internal override ICommand Parse(string userInput)
         {
             string[] inputArguments = SplitInputArguments(userInput);
-            string commandName = $"{inputArguments[0]}{inputArguments[1]}".ToLower();
+            string commandName = $"{inputArguments[0]} {inputArguments[1]}".ToLower();
             List<string> commandOptions = ParseCommandOptions(inputArguments);
-            Enum.TryParse(commandName, true, out AvailableCommands command);
             ICommand parsedCommand;
-            switch (command)
+            switch (commandName)
             {
-                case AvailableCommands.FileDelete:
+                case fileDeleteCommand:
                     parsedCommand = new DeleteFile(_fileService, commandOptions);
                     break;
-                case AvailableCommands.FileDownload:
+                case fileDownloadCommand:
                     parsedCommand = new DownloadFile(_fileService, commandOptions);
                     break;
-                case AvailableCommands.FileInfo:
+                case fileInfoCommand:
                     parsedCommand = new InfoFile(_fileService, commandOptions);
                     break;
-                case AvailableCommands.FileMove:
+                case fileMoveCommand:
                     parsedCommand = new MoveFile(_fileService, commandOptions);
                     break;
-                case AvailableCommands.FileUpload:
+                case fileUploadCommand:
                     parsedCommand = new UploadFile(_fileService, commandOptions);
                     break;
-                case AvailableCommands.UserInfo:
+                case userInfoCommand:
                     parsedCommand = new InfoUser(_fileService);
                     break;
                 default: throw new Exception("Non-existent command!");
